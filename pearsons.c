8e8a1b5afe9735d474cd78860e716760037a39b7
Mousa Shaikh-Soltan <sgmshaik@liverpool.ac.uk>
2017-10-24 14:11:21 +0100

    standard dev and mean

--- pearsons.c
+++ pearsons.c
@@ -1,18 +1,34 @@
 #include <stdio.h>
 #include <math.h>
-
-double mean_func(double *data)
+#include <stdlib.h>
+double mean_func(double *data, int size)
 {
 
-double mean = 0.0;
-
+	double mean = 0.0;
+	for(int i = 0; i < size ; size ++)
+	{
+		
+	mean += data[i]; 
+	
+	}
+mean /= size;
 return mean;
 }
 
-double sd_func(double *data, double l_mean)
+double sd_func(double *data, double l_mean, int size)
 {
 
-double sd = 0.0;
+	double sd = 0.0;
+	
+	for(int i = 0; i < size ; size ++)
+	{
+	
+	sd += (data[i] - l_mean)*(data[i] - l_mean)
+		
+	}
+
+	
+	sd /= size;
 
 return sd;
 }
@@ -20,16 +36,32 @@ return sd;
 double pxy_func(double *X, double *Y, double sdy, double sdx)
 {
 
-double pxy = 0.0;
+	double pxy = 0.0;
 
-return pxy;
+	return pxy;
 
 }
 
-void dataInit( double *data, int shift  )
+void dataInit( double *data, int shift, int size )
 {
 
-data = 
+	data = malloc(sizeof(double)*size);
+
+	for(int i = 0; i<size; i++)
+	{
+		data[i] = sin(i+shift);
+			
+	}
+
+
+
+}
+
+void free_data(double *X, double *Y)
+{
+
+	free(X);
+	free(Y);
 
 }
 
