8c1371cb27e332eda3a7029588be987573bac8ac
Mousa Shaikh-Soltan <sgmshaik@liverpool.ac.uk>
2017-10-24 13:35:15 +0100

    skeleton code

---
+++ pearsons.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <math.h>
+
+double mean_func(double *data)
+{
+
+double mean = 0.0;
+
+return mean;
+}
+
+double sd_func(double *data, double l_mean)
+{
+
+double sd = 0.0;
+
+return sd;
+}
+
+double pxy_func(double *X, double *Y, double sdy, double sdx)
+{
+
+double pxy = 0.0;
+
+return pxy;
+
+}
+
+void dataInit( double *data, int shift  )
+{
+
+data = 
+
+}
+
+int main()
+{
+
+
+
+return 1;
+}
